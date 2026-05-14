468b1d79cc75d831e4b8e25bbdc6e588129bebb9
Amine-Rjab <rejebmohamedamine9@gmail.com>
2026-05-14 23:49:07 +0100

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def run_command(user_input):
 def login(username, password):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-
+    # sql injection
     query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
 
     cursor.execute(query)
